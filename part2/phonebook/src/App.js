b44611457a2ff03bc8694ff66923ff6e48189695
Alla Rakhimova <alla.rakhimova103@gmail.com>
2019-12-27 18:56:44 +0200

    homework 2b

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,19 +2,28 @@ import React, {useState} from 'react';
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-1234567'},
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ])
   const [newName, setNewName ] = useState('') //The newName state is meant for controlling the form input element.
+  const [newNumber, setNewNumber] = useState('')
+  const [filteredList, setFilter] = useState('')
+    
+ const namesToShow = persons.filter(el =>
+  el.name.toLowerCase().startsWith(filteredList.toLowerCase()))
 
-  const rows = () => persons.map(person =>
-    <li key={person.name}>{person.name}</li>
+  const rows = () => namesToShow.map(person =>
+    <li key={person.name}>{person.name} {person.number}</li>
     ) 
 
   const addPerson = (event) => {
     event.preventDefault()
 
     const personObject = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
     persons.forEach(x => {
@@ -24,21 +33,40 @@ const App = () => {
       else {
         setPersons(persons.concat(personObject))
         setNewName('')
+        setNewNumber('')
       }
     })
   }
 
+  //эти хэндлеры чтобы записать то, что вводят в форму в новое состояние 
   const handlePersonChange = (event) => {
-    console.log(event.target.value)
+    //console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    //console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
+  const handleFilterChange = (event) => {
+    console.log(event.target.value)
+    setFilter(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={filteredList} onChange={handleFilterChange} />
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={addPerson}>
         <div>
           name: <input value={newName} onChange={handlePersonChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberChange} />
         </div>
         <div>
           <button type="submit">add</button>
@@ -52,7 +80,3 @@ const App = () => {
 }
 
 export default App;
-
-/*Sometimes it can be useful to render state and other variables as text for debugging purposes. You can temporarily add the following element to the rendered component:
-
-<div>debug: {newName}</div>*/
